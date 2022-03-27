cea62958744f8cced931b3958b49c2e4165bdfb1
simran0963 <simranmehta0963@gmail.com>
2022-03-28 01:42:25 +0530

    mean sd

---
+++ TCS/mar28/mean_sd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#define SIZE 1000
+
+int main()
+{
+	float arr[SIZE], sum=0.0, mean, SD;
+	int n, i;
+	printf("Enter the number of elements: \n");
+	scanf("%d", &n);
+	printf("Enter the value of elements: \n");
+	for(i=0; i<n; i++)
+	scanf("&d", arr[i]);
+
+	for(i=0; i<n; i++)
+	sum+=arr[i];
+	mean=sum/n;
+
+	sum=0.0;
+	for(i=0; i<n; i++)
+	sum+=(arr[i]-mean)*(arr[i]-mean);
+	SD = sqrt(sum/n);
+	printf("The mean and SD of the numbers are %d and %d", mean, SD);
+}
