cfa587822a8a7799f5e2a2f5a130c753897361c6
simran0963 <simranmehta0963@gmail.com>
2022-03-28 15:32:57 +0530

    matrices product

--- TCS/mar28/product matrices.c
+++ TCS/mar28/product matrices.c
@@ -2,7 +2,14 @@
 
 int main()
 {
+	int arr[];
 	int n, m, i, j;
+	printf("Enter the number of rows in the I matrix: \n");
+	scanf("%d", &m);
+	printf("Enter the number of columns in I matrix: \n");
+	scanf("%d", &n);
 	for(i=0; i<n; i++)
 	for(j=0; j<m; j++)
+	scanf("%d", arr[i][j]);
+
 }
