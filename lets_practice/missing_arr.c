9f9503d848e33716d62531874bf7e164f3dc8200
simran0963 <simranmehta0963@gmail.com>
2022-04-20 18:17:36 +0530

    missing num

--- lets_practice/missing_arr.c
+++ lets_practice/missing_arr.c
@@ -2,37 +2,33 @@
 
 int main()
 {
-	int n, m, a[100], b[100], i, j, miss, x=0;
-	printf("Enter the number of elements in I array: ");
-	scanf("%d",&n);
-	printf("Enter the elements: \n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	} 
-	printf("Enter the number of elements in II array: ");
-	scanf("%d",&m);
-	printf("Enter the elements: \n");
-	for(i=0;i<m;i++)
-	{
-		scanf("%d",&b[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-		x=0;
-		for(j=0;j<m;j++)
-		{
-			if(b[j]==a[i])
-			{
-				x++;
-			}
-			if(j==m && x==0)
-			{
-				miss=a[i];
-			}
-			
-		}
-	} 
-	printf("The missing element in the II array is : %d", miss);
-	return 0;
+    int n, a[100], b[100], i, j, x;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    printf("Enter the elements of the first array:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the elements of the second array:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        x=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==b[j])
+            {
+                x++;
+            }
+        }
+        if(x==0)
+        {
+            printf("The number missing in the second array is : %d",a[i]);
+        }
+    }
+    return 0;
 }
