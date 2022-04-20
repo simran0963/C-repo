da6ba7552f6ba0cf980c8d5557ae366f0622a47a
simran0963 <simranmehta0963@gmail.com>
2022-04-20 19:52:09 +0530

    finding the missing element

--- lets_practice/find_miss_arr.c
+++ lets_practice/find_miss_arr.c
@@ -2,36 +2,36 @@
 
 int main()
 {
-	int n, arr[100], i, num, j, temp;
-	printf("Enter the number of elements: ");
-	scanf("%d",&n);
-	printf("Enter the elements : \n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	for(i=0;i<n-1;i++)
-	{
-		for(j=0;j<n-i-1;j++)
-		{
-			if(arr[j]>arr[j+1])
-			{
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-		for(i=0;i<n;i++)
-		{
-			printf("%d  ",arr[i]);
-		}
-		for(i=0;i<n;i++)
-		{
-			if(arr[i]!=i+1)
-			{
-				printf("\nThe missing number is : %d",i+1);
-				return i+1;
-			}
-		}
-	}
+    int n, arr[100], i, j, temp, num;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    printf("Enter the elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        temp=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>temp)
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=temp;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]-arr[i-1]!=1)
+        {
+            printf("\nThe missing number is : %d",i);
+            return 0;
+        }
+    }
+    return 0;
 }
