43829d859908571d8110a62322e4ba1bcecad68f
simran0963 <simranmehta0963@gmail.com>
2022-04-26 03:42:25 +0530

    vowel consonants

---
+++ lets_practice/String/vow_con.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX 100
+int main()
+{
+	int i, n, len, vo=0, co=0, wo=0, sp=0;
+	char str[MAX], caps[MAX], vowel[MAX], consonant[MAX];
+	printf("Enter the string: ");
+	fgets(str,MAX,stdin);
+	n=strlen(str);
+	for(i=0;i<n;i++)
+	{
+		caps[i]=toupper(str[i]);
+	}
+	for(i=0;i<strlen(caps);i++)
+	{
+		if(caps[i]=='A'||caps[i]=='E'||caps[i]=='I'||caps[i]=='O'||caps[i]=='U')
+		{
+			vowel[vo]=caps[i];
+			++vo;
+		}
+		else if(caps[i]==' ')
+		{
+			++sp;
+		}
+	}	co=n-vo-sp;
+	printf("Total length : %d\nTotal words : %d\nVowels : %d\nConsonants : %d\nSpaces : %d",n-1,sp+1,vo,co-1,sp);
+}
