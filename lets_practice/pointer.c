3607aa7dd2558e5b8014af1f3561a52c5212563f
simran0963 <simranmehta0963@gmail.com>
2022-05-06 11:19:23 +0530

    pointer basics

--- lets_practice/pointer.c
+++ lets_practice/pointer.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
 #include<string.h>
-#define SIZE 100
-void fun(int n){
-	n=20;
-
-}
 
 int main()
 {
 	int a=10;
 	int *p;
 	p = &a;
-	printf("\n%u",&a); //prints address
-	printf("\n%d",p);  //prints address
-	printf("\n%d",*p);  //prints value on that address
-	fun(a);
+	printf("\nAddress of a is : %u",&a); //%u is unsigned int format specifier
+	printf("\nAddress of ptr is : %x",&p); //%x is unsigned hexadecimal for int
+	printf("\nThe value in a is : %d",a);
+	printf("\nValue in ptr is : %p",p);  //%p is for printing address for ptr and var
+	printf("\nValue pointed by ptr is : %d",*p);
 }
